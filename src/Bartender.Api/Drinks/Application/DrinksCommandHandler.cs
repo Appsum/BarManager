25c98564b571fb5e542cc510ca33eb94dde9c909
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-20 21:29:35 +0100

    Implement other command handlers

--- src/Bartender.Api/Drinks/Application/DrinksCommandHandler.cs
+++ src/Bartender.Api/Drinks/Application/DrinksCommandHandler.cs
@@ -1,8 +1,15 @@
-﻿using Bartender.Api.Drinks.Domain.Repositories;
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Bartender.Api.Drinks.Application.Commands;
+using Bartender.Api.Drinks.Domain;
+using Bartender.Api.Drinks.Domain.Repositories;
+
+using MediatR;
 
 namespace Bartender.Api.Drinks.Application
 {
-    public class DrinksCommandHandler
+    public class DrinksCommandHandler : ICommandHandler<AddDrink>, ICommandHandler<RenameDrink>, ICommandHandler<DeleteDrink>, ICommandHandler<OrderDrinks>
     {
         private readonly IDrinksRepository _drinksRepository;
 
@@ -10,5 +17,17 @@ namespace Bartender.Api.Drinks.Application
         {
             _drinksRepository = drinksRepository;
         }
+
+        public async Task<Unit> Handle(AddDrink request, CancellationToken cancellationToken)
+        {
+            await _drinksRepository.Add(new Drink(request.Name));
+            return Unit.Value;
+        }
+
+        public async Task<Unit> Handle(RenameDrink request, CancellationToken cancellationToken) => throw new System.NotImplementedException();
+
+        public async Task<Unit> Handle(DeleteDrink request, CancellationToken cancellationToken) => throw new System.NotImplementedException();
+
+        public async Task<Unit> Handle(OrderDrinks request, CancellationToken cancellationToken) => throw new System.NotImplementedException();
     }
 }
