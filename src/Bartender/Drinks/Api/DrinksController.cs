54d6c0f026ee0b39270240fc5655dac3e209a822
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-21 09:18:51 +0100

    Implement IMediator in the DrinksController

--- src/Bartender/Drinks/Api/DrinksController.cs
+++ src/Bartender/Drinks/Api/DrinksController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Bartender.Drinks.Api.Models;
+using Bartender.Drinks.Application.Commands;
+using Bartender.Drinks.Application.Queries;
 using Bartender.Drinks.Domain;
+
+using MediatR;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,13 +18,18 @@ namespace Bartender.Drinks.Api
     [Route("api/[controller]")]
     public class DrinksController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public DrinksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
         [HttpGet] // GET api/drinks
         [ProducesResponseType(typeof(ICollection<Drink>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAll()
         {
-            await Task.CompletedTask;
-
-            return Ok();
+            IReadOnlyCollection<Drink> drinks = await _mediator.Send(new GetAllDrinks());
+            return Ok(drinks);
         }
 
         [HttpGet("{id}")] // GET api/drinks/{id}
@@ -33,23 +42,21 @@ namespace Bartender.Drinks.Api
                 return BadRequest($"Id may not be {Guid.Empty}");
             }
 
-            await Task.CompletedTask;
-
-            return Ok();
+            Drink drink = await _mediator.Send(new GetDrinkById(id));
+            return Ok(drink);
         }
 
         [HttpPost] // POST api/drinks
-        [ProducesResponseType(typeof(Drink), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Create([FromBody] CreateDrinkDto createDrinkDto)
         {
-            await Task.CompletedTask;
-
+            await _mediator.Send(new CreateDrink(createDrinkDto.Name));
             return Ok();
         }
 
         [HttpPut("{id}")] // PUT api/drinks/{id}
-        [ProducesResponseType(typeof(Drink), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Rename([FromRoute] Guid id, [FromBody] RenameDrinkDto renameDrinkDto)
         {
@@ -58,8 +65,7 @@ namespace Bartender.Drinks.Api
                 return BadRequest($"Id may not be {Guid.Empty}");
             }
 
-            await Task.CompletedTask;
-
+            await _mediator.Send(new RenameDrink(id, renameDrinkDto.NewName));
             return Ok();
         }
 
@@ -68,7 +74,7 @@ namespace Bartender.Drinks.Api
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> OrderDrinks([FromBody] OrderDrinksDto orderDrinksDto)
         {
-            await Task.CompletedTask;
+            await _mediator.Send(new OrderDrinks(orderDrinksDto.DrinksOrder));
 
             return Ok();
         }
@@ -83,7 +89,7 @@ namespace Bartender.Drinks.Api
                 return BadRequest($"Id may not be {Guid.Empty}");
             }
 
-            await Task.CompletedTask;
+            await _mediator.Send(new DeleteDrink(id));
 
             return Ok();
         }
