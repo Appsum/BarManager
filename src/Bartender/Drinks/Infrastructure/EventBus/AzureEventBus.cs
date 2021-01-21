54b78bf3e73d11fd418ee645c16eec5ceeae908c
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-21 14:15:02 +0100

    Implement AzureEventBus

---
+++ src/Bartender/Drinks/Infrastructure/EventBus/AzureEventBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bartender.Drinks.Infrastructure.EventBus
+{
+    public class AzureEventBus
+    {
+        
+    }
+}
