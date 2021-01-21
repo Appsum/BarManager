161ec38d4379b143e7fad6421ec055126b894c69
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-21 08:45:45 +0100

    OrderDrink command uses the application dto

--- src/Bartender/Drinks/Application/Commands/OrderDrink.cs
+++ src/Bartender/Drinks/Application/Commands/OrderDrink.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 
+using Bartender.Drinks.Application.Commands.Dtos;
+
 namespace Bartender.Drinks.Application.Commands
 {
     public class OrderDrinks
     {
-        public ICollection<(string name, int amount)> Orders { get; }
-        public OrderDrinks(ICollection<(string name, int amount)> orders)
+        public ICollection<OrderDrinkDto> Orders { get; }
+        public OrderDrinks(ICollection<OrderDrinkDto> orders)
         {
             Orders = orders;
         }
