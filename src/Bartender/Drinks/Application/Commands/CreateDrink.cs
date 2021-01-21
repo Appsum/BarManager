5a9e9eb62b94c3ce883930de3544bc85d175552a
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-21 09:14:02 +0100

    Rename AddDrink to CreateDrink to match other namings

---
+++ src/Bartender/Drinks/Application/Commands/CreateDrink.cs
@@ -0,0 +1,12 @@
+﻿namespace Bartender.Drinks.Application.Commands
+{
+    public class CreateDrink
+    {
+        public CreateDrink(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
