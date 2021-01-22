a4e7cde5a50b0b45b32bdfb0bf68d04cd35db4a1
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-22 16:33:22 +0100

    Blob storage implementation in the handler

--- src/Waitress/Handlers/ReceiptNotificationHandler.cs
+++ src/Waitress/Handlers/ReceiptNotificationHandler.cs
@@ -2,17 +2,31 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+using Azure.Storage.Blobs;
+
 using MediatR;
 
+using Microsoft.Extensions.Options;
+
+using Waitress.Configuration;
 using Waitress.Events;
 
 namespace Waitress.Handlers
 {
     public class ReceiptNotificationHandler : INotificationHandler<OrderPlacedMessageReceived>
     {
-        public Task Handle(OrderPlacedMessageReceived notification, CancellationToken cancellationToken)
+        private readonly BlobContainerClient _blobContainerClient;
+
+        public ReceiptNotificationHandler(BlobServiceClient blobServiceClient, IOptions<BlobStorageSettings> blobStorageSettingsOptions)
         {
-            return Task.CompletedTask;
+            _blobContainerClient = blobServiceClient.GetBlobContainerClient(blobStorageSettingsOptions.Value.ContainerName);
+        }
+
+        public async Task Handle(OrderPlacedMessageReceived notification, CancellationToken cancellationToken)
+        {
+            var fileName = $"receipt-{DateTime.Now:yyyyMMddHHmmss}.csv";
+            await _blobContainerClient.UploadBlobAsync(fileName, notification.AsCsvStringStreamForBlobStorage(), cancellationToken);
+
         }
     }
 }
