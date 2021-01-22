bb3b3861da57996f82a5ed4239cc0f0a7e19c2ab
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-22 12:36:37 +0100

    Fix the Hub

--- src/Waitress/TablesHub.cs
+++ src/Waitress/TablesHub.cs
@@ -1,14 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 
 namespace Waitress
 {
-    public class TablesHub : Hub
-    {
-        public async Task DeliverOrder(IDictionary<string, int> order)
-            // The Deliver method parameter MUST EXACTLY match the method which the client is listening on
-            => await Clients.All.SendAsync("Deliver", order);
-    }
+    public class TablesHub : Hub { }
 }
