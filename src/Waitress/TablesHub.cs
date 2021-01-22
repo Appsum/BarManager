112cb96d75bc8e7efff7f11fcdea095922f242ff
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-22 11:30:42 +0100

    Add hub

---
+++ src/Waitress/TablesHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Waitress
+{
+    public class TablesHub : Hub
+    {
+        public async Task DeliverOrder(IDictionary<string, int> order)
+            // The Deliver method parameter MUST EXACTLY match the method which the client is listening on
+            => await Clients.All.SendAsync("Deliver", order);
+    }
+}
