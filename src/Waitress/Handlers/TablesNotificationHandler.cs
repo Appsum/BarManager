e52ac6b3ae8f55678d59b4b9caa0d1fa9e03d9d5
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-22 12:35:58 +0100

    Implement handler

--- src/Waitress/Handlers/TablesNotificationHandler.cs
+++ src/Waitress/Handlers/TablesNotificationHandler.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 using MediatR;
+
+using Microsoft.AspNetCore.SignalR;
 
 using Waitress.Events;
 
@@ -10,9 +11,16 @@ namespace Waitress.Handlers
 {
     public class TablesNotificationHandler : INotificationHandler<OrderPlacedMessageReceived>
     {
-        public Task Handle(OrderPlacedMessageReceived notification, CancellationToken cancellationToken)
+        private readonly IHubContext<TablesHub> _tablesHub;
+
+        public TablesNotificationHandler(IHubContext<TablesHub> tablesHub)
         {
-            return Task.CompletedTask;
+            _tablesHub = tablesHub;
+        }
+        public async Task Handle(OrderPlacedMessageReceived notification, CancellationToken cancellationToken)
+        {
+            // The Deliver method parameter MUST EXACTLY match the method which the client is listening on
+            await _tablesHub.Clients.All.SendAsync("Deliver", notification.OrderedDrinks, cancellationToken);
         }
     }
 }
