b476504427c09f482d4741b24cc69c6f9f74546e
Nils Goovaerts <nils.goovaerts@EGSSIS.COM>
2021-01-22 09:05:08 +0100

    Implement the IEventBus

--- src/Bartender/Drinks/Infrastructure/EventBus/AzureEventBus.cs
+++ src/Bartender/Drinks/Infrastructure/EventBus/AzureEventBus.cs
@@ -1,13 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+
+using Bartender.Drinks.Application.EventBus;
+
+using Microsoft.Azure.ServiceBus;
 
 namespace Bartender.Drinks.Infrastructure.EventBus
 {
-    public class AzureEventBus
+    public class AzureEventBus : IEventBus
     {
-        
+        private readonly ITopicClient _topicClient;
+
+        public AzureEventBus(ITopicClient topicClient)
+        {
+            _topicClient = topicClient;
+        }
+        public async Task Publish(IMessage message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            
+            byte[] payload = JsonSerializer.SerializeToUtf8Bytes(message, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            var serviceBusMessage = new Message(payload);
+            await _topicClient.SendAsync(serviceBusMessage);
+        }
     }
 }
